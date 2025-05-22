220752364f83a69fc9861ea3127554eabb4666e3
Wilson Martinez <133081999+rslcia11@users.noreply.github.com>
2025-05-21 20:38:27 -0500

    21/05/2023

---
+++ semana_03/practica/wimartinezji/lib/presentation/counter_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen>createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Center(child: Text('$clickCounter', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w300),)),
+          Text('Click${clickCounter == 1 ? '':'s'}', style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),)
+        ],),
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          
+          setState(() {
+            clickCounter++;
+          });
+        }, child: Icon(Icons.plus_one),),
+      );
+  }
+}
